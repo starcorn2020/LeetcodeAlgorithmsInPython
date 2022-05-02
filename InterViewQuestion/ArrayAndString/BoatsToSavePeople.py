7bd39be82f2375b7314b93558266c244a90383b6
starcorn2020 <starcorn2020@gmail.com>
2022-05-02 16:15:20 +0800

    boatsSavePeople

---
+++ InterViewQuestion/ArrayAndString/BoatsToSavePeople.py
@@ -0,0 +1,18 @@
+# Explanation - Boats to save people - Medium
+
+# People : people array, indes is people's index , value is people's weight .
+# Limit : the limit of boats max weight cap.
+
+# People : [1,2]
+# Limit : 3 
+# Answer : 1
+
+# People : [3,2,2,1]
+# Limit : 3 
+# Answer : 3
+
+# Weight Limit : 
+# 1. No weight more than boat's limit
+# 2. No weight less than 0 
+
+
