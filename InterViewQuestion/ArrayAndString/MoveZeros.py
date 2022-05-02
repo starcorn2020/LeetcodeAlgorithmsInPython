59572eb9c74ff7dbcc7e6532cd876cdf6409f981
starcorn2020 <starcorn2020@gmail.com>
2022-05-02 15:46:05 +0800

    move zeros

---
+++ InterViewQuestion/ArrayAndString/MoveZeros.py
@@ -0,0 +1,38 @@
+# Explanation - Move Zeroes Brute force Intuition - Easy
+
+# Input : [0,1,0,3,12]
+# Output : [1,3,12,0,0]
+
+'''
+Example
+
+moveZeroes(input){
+    output = []
+    n = input.length
+    Loop as i in range(0,n){
+        if(input[i] is non-zero){
+            output.add(input[i])
+        }
+    }
+    m = output.length
+    Loop in range(m,n){
+        output.add(0)
+    }
+}
+
+'''
+
+def moveZeros(inputArr):
+
+    outputArr = [i for i in inputArr if i != 0]
+
+    for i in range(len(outputArr),len(inputArr)):
+        outputArr.append(0)
+
+    return outputArr
+
+if __name__ == '__main__':
+
+    arr = [0,1,0,3,12]
+    sortedArr = moveZeros(arr)
+    print(sortedArr)
